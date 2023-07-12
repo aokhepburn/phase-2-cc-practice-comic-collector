fe75322c3767bf53b0ac0385b967002516d257d0
Alice Hepburn <aliceokhepburn@gmail.com>
2023-07-12 01:01:47 -0400

    DELIVERABLE TWO PASSED Comics.js updated to have a functional toggle between comic cover and comic details.

--- src/components/Comic.js
+++ src/components/Comic.js
@@ -1,16 +1,23 @@
+import {useState} from 'react'
+
 function Comic({image, title, issue}) {
 console.assert(title !== null)
 
+const [imageOrDetails, setImageOrDetails]= useState('true')
+
+function handlesImageOrDetailsEvent(){
+  setImageOrDetails(!imageOrDetails)
+}
+
   return (
-    <div className="comic-item">
+    <div className="comic-item" onClick={()=>handlesImageOrDetailsEvent()}>
 
-      {/* The image should render if the details aren't displayed */}
-      <img src={image} alt={title} />
-
-      {/* The details should render if the image isn't displayed */}
-      <h3>{title}</h3>
+{imageOrDetails ? 
+      <img src={image} alt={title} /> :
+      <div><h3>{title}</h3>
       <h4>{issue}</h4>
       <button>Remove</button>
+      </div>}
 
     </div>
   )
