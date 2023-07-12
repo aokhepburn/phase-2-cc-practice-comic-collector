072b05012a87a09b6ba33c5d0f4076e902f29343
Alice Hepburn <aliceokhepburn@gmail.com>
2023-07-12 02:02:25 -0400

    DELIVERABLE 3 ComicForm.js & App.js updates for tracking form input. Is set up to start making post event handler.

--- src/components/App.js
+++ src/components/App.js
@@ -6,11 +6,21 @@ function App() {
   //create state to be populated with json data and future filters
   const [comics, setComics] = useState([])
 
+  //sets the states for form inputs to create a newComic object
+  const [newImage, setsNewImage] = useState('')
+  const [newTitle, setsNewTitle] = useState('')
+  const [newIssue, setsNewIssue] = useState(0)
+  const [newDescription, setsNewDescription] = useState('')
+
   useEffect(()=>(
     fetch('http://localhost:8004/comics/')
     .then(res=>res.json())
     .then(data=>setComics(data))
   ), [])
+  
+  //POST either in an event handler or in another function that is called by the event handler
+  //do not forget preventDefault or that you do not close the paranthese on the fetch until AFTER the JSON.stringify({newComic})
+
 
   return (
     <div className="App">
@@ -24,14 +34,24 @@ function App() {
         </div>
 
         <div className="sidebar">
-          <ComicForm />
+          <ComicForm 
+          image={newImage}
+          setsImage={setsNewImage}
+          title={newTitle}
+          setsTitle={setsNewTitle}
+          issue={newIssue}
+          setsIssue={setsNewIssue}
+          description={newDescription}
+          setsDescription={setsNewDescription}/>
         </div>
 
       </div>
 
 
     </div>
-  );
+  )
 }
 
 export default App;
+
+// newImage url https://images2.fanpop.com/images/photos/2900000/Crisis-dc-comics-2970689-1024-1616.jpg
--- src/components/ComicForm.js
+++ src/components/ComicForm.js
@@ -1,21 +1,34 @@
-function ComicForm() {
+function ComicForm({image, setsImage, title, setsTitle, issue, setsIssue, description, setsDescription}) {
+  
+  //handleSubmit will become POST in APP component this is just for working through all the components\
+  //obviously getting rid of the extra props (the variable part of the states) but useful for console.log now
+  //left at Wed Jul 12 01:50
+  //inputs are changing the states, proven by console.log on line23
+  //handleSubmit function is getting the right target value, so that can be passed through to a post object
+  function handleSubmit(e){
+    e.preventDefault()
+    console.assert(image != '', 'testing image value input', image)
+    e.target.reset()
+  }
+  
   return (
 
-    <form className="comic-form">
+    <form className="comic-form" onSubmit={(e)=>handleSubmit(e)}>
 
       <h2>Add A New Issue</h2>
 
       <label htmlFor="image_url">Image URL: </label>
-      <input name="image_url" />
+      <input name="image_url" value='' onChange={(e)=>setsImage(e.target.value)}/>
 
       <label htmlFor="title">Title: </label>
-      <input name="title" />
+      <input name="title" value='' onChange={(e)=>setsTitle(e.target.value)}/>
+      {console.log(title)}
 
       <label htmlFor="issue">Issue Number: </label>
-      <input name="issue" type="number" />
+      <input name="issue" type="number" value='' onChange={(e)=>setsIssue(e.target.value)}/>
 
       <label htmlFor="description">Description: </label>
-      <input name="description" />
+      <input name="description"  value='' onChange={(e)=>setsDescription(e.target.value)}/>
 
       <input type="submit" value="Add Issue" />
 
